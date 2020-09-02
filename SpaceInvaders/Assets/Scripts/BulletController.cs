66aa92150cb0aa2b3504e2a70f82c68d64af27c5
martinsspn <martinsspn@gmail.com>
2020-09-02 18:24:55 -0300

    firstCommit

---
+++ SpaceInvaders/Assets/Scripts/BulletController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    private Transform bullet;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bullet = GetComponent<Transform> ();
+    }
+
+    void FixedUpdate()
+    {
+        bullet.position += Vector3.up * speed;
+
+        if (bullet.position.y >= 10)
+            Destroy (gameObject);
+    }
+    // Update is called once per frame
+    void OnTriggerEnter2d(Collider2D other){
+        if(other.tag == "Enemy"){
+            Destroy (other.gameObject);
+            Destroy (gameObject);
+        }else if(other.tag == "Base"){
+            Destroy (gameObject);
+        }
+    }
+}
---
+++ SpaceInvaders/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+	private Transform player;
+	public float speed;
+	public float maxBound, minBound, maxUp, minDown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Transform> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis ("Horizontal");
+        float v = Input.GetAxis ("Vertical");
+        if (player.position.x < minBound && h < 0 )
+        	h = 0;
+        else if (player.position.x > maxBound && h > 0)
+        	h = 0;
+
+        player.position += Vector3.right * h * speed;
+        
+        if (player.position.y > minDown)
+            player.position = ();
+        	
+
+        
+    }
+}

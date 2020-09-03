ec969c7094e6a9d814e56877f12d459d637ef8c3
martinsspn <martinsspn@gmail.com>
2020-09-02 23:57:58 -0300

    another commit

--- SpaceInvaders/Assets/Scripts/EnemyController.cs
+++ SpaceInvaders/Assets/Scripts/EnemyController.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class EnemyController : MonoBehaviour
 {
+    private Transform enemy;
+    public float speed;
+    public float next_spawn_time;
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemy = GetComponent<Transform> ();
+        next_spawn_time = Time.time+5.0f;
+
     }
 
+
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        enemy.position += Vector3.up * speed * -1;
+        if(enemy.position.y <= -7f){
+            Destroy (gameObject);
+        }
+    }
+
+    void Update(){
+        if(Time.time > next_spawn_time)
+     {
+         //do stuff here (like instantiate)
+        Instantiate(enemy, Vector3.right * 2f, enemy.rotation);
+        //increment next_spawn_time
+        next_spawn_time += 5.0f;
+     }
     }
 }

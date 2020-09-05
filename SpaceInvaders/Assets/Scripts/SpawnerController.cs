eb922e4f2116738d884722897d6ef276005a9113
martinsspn <martinsspn@gmail.com>
2020-09-05 18:14:06 -0300

    adicionando sprites

---
+++ SpaceInvaders/Assets/Scripts/SpawnerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject enemy;
+    public float minX;
+    public float maxX;
+    
+    public float timeSpawn;
+    private bool spawned;
+    void Start()
+    {
+        spawned = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(!spawned)
+            StartCoroutine(TimedSpawn(timeSpawn));
+
+            //InvokeRepeating("Spawn", timeSpawn, timeSpawn);
+    }
+
+    public void Spawn()
+    {
+        GameObject ob = GameObject.Instantiate(enemy) as GameObject;
+        Vector2 newPos = new Vector2(Random.Range(minX, maxX), 7f);
+        ob.transform.position = newPos;
+    }
+      
+    IEnumerator TimedSpawn(float t)
+    {
+        spawned = true;
+        yield return new WaitForSeconds(t);
+        Spawn();
+        spawned = false;
+    }
+}

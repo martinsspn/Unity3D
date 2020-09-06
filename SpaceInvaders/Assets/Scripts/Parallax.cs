f8ca58bd7b4ff62dc51ed62f6a796c9f184848a6
martinsspn <martinsspn@gmail.com>
2020-09-05 21:03:06 -0300

    primeira tentativa de parallax

---
+++ SpaceInvaders/Assets/Scripts/Parallax.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//O script você pode pôr em qualquer lugar da cena, desde que esteja ativo
+//Não se esqueça de nomear seu script para "Image", ou se for mudar, mude também no script.
+public class Parallax : MonoBehaviour
+{
+    //Coloque a referência de background aqui.
+    public RawImage image;
+    
+    //Mude a velocidade que sua imagem irá "andar".
+    public float speed;
+
+    private void FixedUpdate()
+    {
+        image.uvRect = new Rect(image.uvRect.x + speed * Time.deltaTime, image.uvRect.y, image.uvRect.width, image.uvRect.height);
+    }
+}
